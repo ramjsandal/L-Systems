a2ec6108e00b90f091f34d035252b41d50cf257c
ramjsandal <ramjsandal@gmail.com>
2024-06-01 18:27:10 -0400

    init project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Raylib_cs;
+
+namespace game;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Hello, World", 12, 12, 20, Color.Black);
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
