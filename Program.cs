0680b7c716c82a3674f0c9e4e347b282a81af8f2
ramjsandal <ramjsandal@gmail.com>
2024-06-02 17:22:08 -0400

    word generation

--- Program.cs
+++ Program.cs
@@ -1,19 +1,53 @@
 ﻿using Raylib_cs;
+using System.Formats.Asn1;
 
 namespace game;
 
 class Program
 {
+    public static Dictionary<char, string> rules = new Dictionary<char, string>()
+    {
+        { 'X', "F+[[X]-X]-F[-FX]+X" },
+        { 'F', "FF" }
+    };
+
+    string word;
+    string generate()
+    {
+        string nextWord = "";
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            var c = word[i];
+            if (rules.ContainsKey(c))
+            {
+                nextWord += rules[c]; 
+            } else
+            {
+                nextWord += c;
+            }
+        }
+
+        return nextWord;
+
+    }
+
     public static void Main()
     {
+        Program program = new Program();
+        program.word = "X";
         Raylib.InitWindow(800, 480, "Hello World");
 
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
-            Raylib.DrawText("Hello, World", 12, 12, 20, Color.Black);
+            Raylib.DrawText(program.word, 12, 12, 20, Color.Black);
             Raylib.EndDrawing();
+            if (Raylib.IsKeyPressed(KeyboardKey.Space))
+            {
+              program.word = program.generate();
+            }
         }
 
         Raylib.CloseWindow();
